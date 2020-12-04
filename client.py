ee80b1f410281e43f5cafad1a75ef90892714f05
mihirjain-iitgn <67909784+mihirjain-iitgn@users.noreply.github.com>
2020-12-04 21:36:41 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,113 +1,133 @@
-import time
-from mininet.net import Mininet
-from mininet.node import Controller, RemoteController, OVSController
-from mininet.node import CPULimitedHost, Host, Node
-from mininet.node import OVSKernelSwitch, UserSwitch
-from mininet.node import IVSSwitch
-from mininet.cli import CLI
-from mininet.log import setLogLevel, info
-from mininet.link import TCLink, Intf
-from subprocess import call
+import socket
+import stdiomask
+import os
+import sys
+import atexit
 
-def myNetwork():
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk),":", end="")
 
-    net = Mininet( topo=None,
-                   build=False,
-                   ipBase='10.0.0.0/8')
+def exit_handler():
+    if (ssid!="None"):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        msg_server = "sessionid(:)"+ssid+"\nstate(:)offline\n"
+        msg_server = str(len(msg_server)) + "\n" + msg_server
+        s.send(msg_server.encode())
+        fd = open(path, "w")
+        fd.write(ssid)
+        fd.close()
+        sys.exit(1)
 
-    info( '* Adding controller\n' )
-    c0=net.addController(name='c0',
-                      controller=Controller,
-                      protocol='tcp',
-                      port=6633)
+class Handle:
+    def __init__(self,message):
+        args = message.split("\n")   
+        args.pop(-1)
+        self.Fields_Req = {"sessionid":"None","state":"None","message":"None","form":"None"}
+        self.Fields_Res = {"sessionid":"None","state":"None","request":"None","form":"None"}
+        self.ExtractFields(args)
 
-    info( '* Add switches\n')
-    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
-    s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
-    s5 = net.addSwitch('s5', cls=OVSKernelSwitch)
-    s6 = net.addSwitch('s6', cls=OVSKernelSwitch)
-    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
-    s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
-    s7 = net.addSwitch('s7', cls=OVSKernelSwitch)
-
-    info( '* Add hosts\n')
-    h7 = net.addHost('h7', cls=Host, ip='10.0.0.7', defaultRoute=None)
-    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
-    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
-    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
-    h9 = net.addHost('h9', cls=Host, ip='10.0.0.9', defaultRoute=None)
-    h8 = net.addHost('h8', cls=Host, ip='10.0.0.8', defaultRoute=None)
-    h6 = net.addHost('h6', cls=Host, ip='10.0.0.6', defaultRoute=None)
-    h5 = net.addHost('h5', cls=Host, ip='10.0.0.5', defaultRoute=None)
-    h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
-
-    info( '* Add links\n')
-
-    net.addLink(h1, s1, bw=40)
-    net.addLink(s2, s1, bw=40)
-    net.addLink(s1, s3, bw=40)
-    net.addLink(s3, s7, bw=20)
-    net.addLink(s3, s6, bw=20)
-    net.addLink(s2, s5, bw=20)
-    net.addLink(s2, s4, bw=20)
-    net.addLink(s4, h2, bw=10)
-    net.addLink(s4, h3, bw=10)
-    net.addLink(s5, h4, bw=10)
-    net.addLink(s5, h5, bw=10)
-    net.addLink(s6, h6, bw=10)
-    net.addLink(s6, h7, bw=10)
-    net.addLink(s7, h8, bw=10)
-    net.addLink(s7, h9, bw=10)
-
-    info( '* Starting network\n')
-    net.build()
-    info( '* Starting controllers\n')
-    for controller in net.controllers:
-        controller.start()
-
-    info( '* Starting switches\n')
-    net.get('s2').start([c0])
-    net.get('s4').start([c0])
-    net.get('s5').start([c0])
-    net.get('s6').start([c0])
-    net.get('s1').start([c0])
-    net.get('s3').start([c0])
-    net.get('s7').start([c0])
-
-    info( '* Post configure switches and hosts\n')
-
-   
-
-   
-    h1.cmd("python server.py &")
-    h2.cmd("python client.py &")
-    h5.cmd("python client.py &")
-    h7.cmd("python client.py &")
-    h9.cmd("python client.py &")
-    time.sleep(10)
+    def splitField(self,entry):
+        entry1, entry2 = entry.split("(:)")
+        return (entry1,entry2)
+        
+    def ExtractFields(self,args):
+        ##Extract values from server message
+        for entry in args:
+            key, value = self.splitField(entry)
+            if key=="form":
+                value = value.split("$")
+            self.Fields_Req[key] = value
     
-    #print(h1.cmd(">"))
-    print(h2.cmd(">"))
-    print(h5.cmd(">"))
-    print(h7.cmd(">"))
-    print(h9.cmd(">"))
+    def printMessage(self):
+        ##Print Message
+        mes = self.Fields_Req["message"].replace("\t","\n")
+        if (mes):
+            os.system('clear')
+            print(mes)
 
-    print("\n\n")
-    #h1.cmd("python ts_thread.py &")
-    #h2.cmd("python tc.py 1 &")
-    #h3.cmd("python tc.py 2 &")
-    #h4.cmd("python tc.py 3 &")
-    #h5.cmd("python tc.py 4 &")
-    #time.sleep(6)
-    #print(h1.cmd(">"))
-    time.sleep(10)
-    #print(h2.cmd(">"))
-    #print(h3.cmd(">"))
-    #print(h4.cmd(">"))
-    #print(h5.cmd(">"))
+    def decide_request(self):
+        ##Taking user request as input
+        global ssid
+        if (self.Fields_Req["sessionid"]!="None"):
+            ssid = self.Fields_Req["sessionid"]
+            if (ssid == "SetNone"):
+                ssid = "None"
+        self.Fields_Res["sessionid"] = ssid
+        self.Fields_Res["state"] = self.Fields_Req["state"]
+        
+        if(self.Fields_Req["form"][0]):
+            if (self.Fields_Req["form"]!="None"):
+                details = ""
+                for i in self.Fields_Req["form"]:
+                    prYellow(i)
+                    if (i!="Password"):
+                        # print(i,":", end="")
+                        ipt =  str(input()).strip()
+                    else:
+                        ipt = stdiomask.getpass(prompt="",mask='*')
+                    while(ipt=="None"):
+                        print("Choose another",i)
+                        ipt =  str(input()).strip()
+                    details = details + ipt + "$"
+                self.Fields_Res["form"] = details
+            else:
+                self.Fields_Res["request"] = str(input("")).strip()
+        
+       
+    def SendMessage(self, server):
+        ##Form message for server
+        self.decide_request()
+        msg_server = ""
+        for key in self.Fields_Res:
+            val = self.Fields_Res[key]
+            if (val!="None" or key=="sessionid"):
+                msg_server += (key + "(:)" + val + "\n")
+        size = len(msg_server)
+        msg_server = str(size) + "\n" + msg_server
+        # print(msg_server)
+        server.send(msg_server.encode())
 
-    CLI(net)
-    net.stop()
+def handleReq(s):
+    ##Handle the response from the server
+    buffer = ""
+    while True:
+        c = s.recv(1).decode()
+        if c=="\n":
+            break
+        buffer += c
+    size = int(buffer)
+    buffer = ""
+    while size>len(buffer):
+        buffer = buffer + s.recv(buffersize).decode()
+    return buffer
 
-setLogLevel( 'info' )
-myNetwork()
+
+def main():
+    ##start client
+   
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    msg_server = "sessionid(:)"+ssid+"\nstate(:)None\n"
+    msg_server = str(len(msg_server)) + "\n" + msg_server
+    s.send(msg_server.encode())
+    buffer = handleReq(s)
+
+    while(1):
+        request = Handle(buffer)
+        request.printMessage()
+        s.close()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        request.SendMessage(s)
+        buffer = handleReq(s)
+
+
+buffersize = 1024
+port = 12345
+host = "127.0.0.1"
+atexit.register(exit_handler)
+path = "./test.txt"
+fd = open(path, "r")
+ssid = fd.readline()
+fd.close()
+main()
